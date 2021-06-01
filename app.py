07abe03f3dd9e923cffba2feaf1fed88f163b950
Vladislav-Ryzhkavets <hitecrime@gmail.com>
2021-06-01 17:43:14 +0300

    new migration/ added security

--- app.py
+++ app.py
@@ -8,6 +8,11 @@ from flask_script import Manager
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 
+from flask_security import SQLAlchemyUserDatastore
+from flask_security import Security
+
+
+
 app = Flask(__name__)
 app.config.from_object(Configuration)
 
@@ -18,8 +23,12 @@ manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
 
-### Admin ###
+# Admin
 from models import *
 admin = Admin(app)
 admin.add_view(ModelView(Post, db.session))
 admin.add_view(ModelView(Tag, db.session))
+
+# Flask-security
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+security = Security(app, user_datastore)
